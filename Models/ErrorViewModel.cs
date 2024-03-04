a03567c262c0b3289ea68cc610cdb6ef119f0fe5
JonasL <46361162+JonasLarsen0@users.noreply.github.com>
2024-03-04 09:52:31 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RallyBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
